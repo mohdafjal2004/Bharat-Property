6647775c6079615561ce902a0a660b78cca53b69
Mohd Afjal <mohdafjalgci2018@gmail.com>
2024-03-30 03:49:50 +0530

    Connect to DB

--- server/index.js
+++ server/index.js
@@ -1,6 +1,14 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("Connected to local DB"))
+  .catch((error) => console.log(error));
+
 const app = express();
 app.listen(5000, () => {
   console.log(`Server is running on 3000`);
-})
- 
+});
