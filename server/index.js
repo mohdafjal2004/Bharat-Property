338ea2069d0046a1f7a8783b02fdc1ae1faa6919
Mohd Afjal <mohdafjalgci2018@gmail.com>
2024-04-05 03:58:09 +0530

    add pre installation

--- server/index.js
+++ server/index.js
@@ -5,19 +5,30 @@ import userRoute from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
 import listingRouter from "./routes/listing.route.js";
 import cookieParser from "cookie-parser";
+import path from "path";
 dotenv.config();
+
 const app = express();
 
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => console.log("Connected to local DB"))
   .catch((error) => console.log(error));
+
+//
+const __dirname = path.resolve();
 app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/user", userRoute);
-app.use("/api/auth", authRouter); 
+app.use("/api/auth", authRouter);
 app.use("/api/listing", listingRouter);
+
+app.use(express.static(path.join(__dirname, "/client/dist")));
+
+app.use("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
